f2f07ff4b2321524f33a5482e1783ff3df8f9752
Luke Olali <mugapedia@gmail.com>
2019-10-08 21:18:52 +0300

    Adds docstrings to constructor method, implements method for accessing array elements

--- array/array.js
+++ array/array.js
@@ -1,8 +1,22 @@
 class MyArray {
 
+  /**
+   * Constructor
+   * @param {number} length - The size of the array
+   * @param {Object} data - Array elemeents
+   */
   constructor(length, data) {
     this.length = 0;
     this.data = {}
+  }
+
+
+  /**
+   * Method to retrieve element from array
+   * @param {number} index - The key of the element in data variable
+   */
+  get(index) {
+    return this.data[index];
   }
 
 }
