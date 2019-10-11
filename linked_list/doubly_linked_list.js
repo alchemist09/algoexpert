a3c6bf94183909360711cc4e360f6d81d3134702
Luke Olali <mugapedia@gmail.com>
2019-10-12 01:45:26 +0300

    Creates the DoublyLinkedList class

--- linked_list/doubly_linked_list.js
+++ linked_list/doubly_linked_list.js
@@ -8,3 +8,16 @@ class NodeDLL {
     this.prev = this.next = null;
   }
 }
+
+class DoublyLinkedList {
+  /**
+   * Coonstructor - Initializes the linked list with a single node
+   * @param {*} value - Value held by the linked list node
+   */
+  constructor(value) {
+    this.head = new NodeDLL(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+}
