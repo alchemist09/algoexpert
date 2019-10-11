5c36a54308d9172328c1568caff8cf1acaae21da
Luke Olali <mugapedia@gmail.com>
2019-10-11 23:10:11 +0300

    Adds method for appending nodes to linked list. Fixes docstring for the constructor of the node class

--- linked_list/linked_list.js
+++ linked_list/linked_list.js
@@ -1,7 +1,7 @@
 class Node {
   /**
    * Constructor
-   * @param {*} Value contained in a node
+   * @param {*} value - Value contained in a node
    */
   constructor(value) {
     this.value = value;
@@ -10,9 +10,26 @@ class Node {
 }
 
 class LinkedList {
+  /**
+   * Constructor
+   * @param {*} value - Value used to initialize the head node
+   */
   constructor(value) {
     this.head = new Node(value);
     this.tail = this.head;
     this.length = 1;
+  }
+
+  /**
+   * Method to append a node to the linked list
+   * @param {*} value - Value going into a linked list node
+   * @returns An instance of the linked list
+   */
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
   }
 }
