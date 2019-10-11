65f39d6995f6e39fd430f5277171b7a2701fa19b
Luke Olali <mugapedia@gmail.com>
2019-10-12 01:36:09 +0300

    Creates node for a doubly-linked list

---
+++ linked_list/doubly_linked_list.js
@@ -0,0 +1,10 @@
+class NodeDLL {
+  /**
+   * Constructor
+   * @param {*} valuue - Value that goes into the linked list node
+   */
+  constructor(value) {
+    this.valuue = value;
+    this.prev = this.next = null;
+  }
+}
