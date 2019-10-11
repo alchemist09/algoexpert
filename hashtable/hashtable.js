f03767d47e91e73a51270e9ef4102fe2cf058cd7
Luke Olali <mugapedia@gmail.com>
2019-10-11 20:59:26 +0300

    Adds method that implement hashing functionality

--- hashtable/hashtable.js
+++ hashtable/hashtable.js
@@ -5,4 +5,17 @@ class HashTable {
   constructor() {
     this.buckets = new Array(50);
   }
+
+  /**
+   * Hash function
+   * @param {string} key - The key that maps to value
+   * @returns {string} hash value
+   */
+  _hash(key) {
+    let hashValue = 0;
+    for(let i=0; i < key.length; i++) {
+      hashValue = (Math.pow(key.charCodeAt(i), 2) + key.charCodeAt(i)) % this.buckets.length;
+    }
+    return hashValue;
+  }
 }
