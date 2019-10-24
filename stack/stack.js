5461ce9b7e2a2ecbf3cb10f2ef891698942360b4
Luke Olali <mugapedia@gmail.com>
2019-10-24 22:32:28 +0300

    Creates Node class used for instantiating nodes going into the stack

---
+++ stack/stack.js
@@ -0,0 +1,10 @@
+/**
+ * Node class
+ * Holds values going into stack for case where stack is implemented using queue
+ */
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
