bbb6bdaa673ef9196b518373aee0816566f99da3
Luke Olali <mugapedia@gmail.com>
2019-10-24 22:42:34 +0300

    Add docstring for Node class constructor, Defines the constructor for Stack class

--- stack/stack.js
+++ stack/stack.js
@@ -3,8 +3,19 @@
  * Holds values going into stack for case where stack is implemented using queue
  */
 class Node {
+  /**
+   * Constructor
+   * @param {*} value 
+   */
   constructor(value) {
     this.value = value;
     this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = this.bottom = null;
+    this.length = 0;
   }
 }
