24e93da9bf0be715d583ef9200832b1e9053b227
Luke Olali <mugapedia@gmail.com>
2019-10-24 22:48:33 +0300

    Adds docstring that explains the implementation approach of the Stack Adds a peek method that checks the top most item in the stack

--- stack/stack.js
+++ stack/stack.js
@@ -13,9 +13,21 @@ class Node {
   }
 }
 
+/**
+ * This class takes a queue based approach to implementing a stack
+ * Pop and push operations are handled by the head of the queue
+ */
 class Stack {
   constructor() {
     this.top = this.bottom = null;
     this.length = 0;
+  }
+
+  /**
+   * Checks and returns the top most item in the stack, but doesn't remove it
+   * @returns {Node} - A node holding sitting at the top of the stack
+   */
+  peek() {
+    return this.top;
   }
 }
