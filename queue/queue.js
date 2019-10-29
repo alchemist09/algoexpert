ab0ba609692b6e4c117aa18c33315f3931805c35
Luke Olali <mugapedia@gmail.com>
2019-10-29 22:52:52 +0300

    Adds method that returns the first element in the queue

--- queue/queue.js
+++ queue/queue.js
@@ -19,4 +19,12 @@ class Queue {
     this.last = null;
     this.length = 0;
   }
+
+  /**
+   * Returns the first element in the queue
+   * @returns {Node} - The first node of the queue
+   */
+  peek() {
+    return this.first;
+  }
 }
