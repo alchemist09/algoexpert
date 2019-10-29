32ea8bd1820f162ff926c7249b2a9b626e094a41
Luke Olali <mugapedia@gmail.com>
2019-10-29 23:19:32 +0300

    Defines method for adding an element to the BST

--- trees/binary_search_tree.js
+++ trees/binary_search_tree.js
@@ -17,4 +17,36 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  /**
+   * Inserts a value to the BST
+   * @param {number} value - Key of the BST node
+   * @returns {BinarySearchTree} - An instance of the BST
+   */
+  insert(value) {
+    const node = new Node(value);
+    if(!this.root) {
+      this.root = node;
+    } else {
+      let current = this.root;
+      while(!!current) {
+        if(node.value < current.value) {
+          if(!current.left) {
+            current.left = node;
+            break;
+          }
+          current = current.left;
+        } else if(node.value > current.value) {
+          if(!current.right) {
+            current.right = node;
+            break;
+          }
+          current = current.right;
+        } else {
+          break;
+        }
+      }
+    }
+    return this;
+  }
 }
