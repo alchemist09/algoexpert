17b91a34a097af36660d646b3d7826b635c64a18
Luke Olali <mugapedia@gmail.com>
2019-10-29 23:11:10 +0300

    Creates the class that defines nodes of the tree

---
+++ trees/binary_search_tree.js
@@ -0,0 +1,11 @@
+class Node {
+  /**
+   * Constructor
+   * @param {*} value - Value in a node of the tree
+   */
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
