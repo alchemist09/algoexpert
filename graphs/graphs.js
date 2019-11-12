1f438963b450b4e0ad4af4200a26979e3f8b40c0
Luke Olali <mugapedia@gmail.com>
2019-11-12 21:16:08 +0300

    Creates the graph class

---
+++ graphs/graphs.js
@@ -0,0 +1,11 @@
+class Graph {
+  /**
+   * constructor
+   * Initializes fields to hold node count, vertices and and edge relationships
+   */
+  constructor() {
+    this.numberOfNodes = 0;
+    this.vertices = [];
+    this.adjacencyList = new Map();
+  }
+}
