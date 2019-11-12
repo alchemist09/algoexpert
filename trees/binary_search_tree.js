96f99619c7104739044f7e2ac5f6869556d849fc
Luke Olali <mugapedia@gmail.com>
2019-11-12 23:49:27 +0300

    Defines function to traverse tree using in-order approach

--- trees/binary_search_tree.js
+++ trees/binary_search_tree.js
@@ -286,6 +286,23 @@ function traversePreOrder(node, list) {
 }
 
 /**
+ * Function that traverses BST using in-order approach
+ * @param {object} node - Tree node from where to begin traversal
+ * @param {array} list - Array to hold nodes as they are being visited
+ * @returns {array} - Array whose elements represent order in which nodes were visited
+ */
+function traverseInOrder(node, list){
+  if(node.left) {
+    traverseInOrder(node.left, list);
+  }
+  list.push(node.value);
+  if(node.right) {
+    traverseInOrder(node.right, list);
+  }
+  return list;
+}
+
+/**
  * Create a Binary Search Tree from sorted Array
  * @param {BinarySearchTree} bst 
  * @param {array} ordered - The sorted collection
