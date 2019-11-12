0fd03bce305461a6e3eb22464e8b2cf3991e9f01
Luke Olali <mugapedia@gmail.com>
2019-11-12 21:34:57 +0300

    Adds code to test out the graph implementation

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -63,3 +63,21 @@ class Graph {
     return string;
   }
 }
+
+const graph = new Graph();
+const vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+for(let i=0; i < vertices.length; i++) {
+  graph.addVertex(vertices[i]);
+}
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('A', 'D');
+graph.addEdge('C', 'D');
+graph.addEdge('C', 'G');
+graph.addEdge('D', 'G');
+graph.addEdge('D', 'H');
+graph.addEdge('B', 'E');
+graph.addEdge('B', 'F');
+graph.addEdge('E', 'I');
+
+// graph.showConnections();
