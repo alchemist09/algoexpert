f4e4f8cd0a568a91b2a49470162d4a56fade6bcc
Luke Olali <mugapedia@gmail.com>
2019-11-18 22:44:38 +0300

    Defines a method for adding a word to the trie

--- trees/trie.js
+++ trees/trie.js
@@ -10,4 +10,25 @@ class Trie {
   constructor() {
     this.root = new TrieNode();
   }
+
+  /**
+   * Iterate through word, mapping each character from word to a path in the trie
+   * If no path exists create one
+   * When iteration is complete indicate that the last character of word
+   * completes a string in the trie
+   */
+  insert(word) {
+    let node = this.root;
+    for(let i=0; i<word.length; i++) {
+      let currentLetter = word[i];
+      if(node.children[currentLetter]) {
+        node = node.children[currentLetter];
+      } else {
+        // create path in trie
+        let newNode = new TrieNode(currentLetter);
+        node.children[currentLetter] = newNode;
+      }
+    }
+    node.end_of_word = true;
+  }
 }
