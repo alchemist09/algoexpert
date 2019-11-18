692c999d8d219851d98a579f60e56f597bb50244
Luke Olali <mugapedia@gmail.com>
2019-11-18 22:50:35 +0300

    Defines method for deleting a word from the trie

--- trees/trie.js
+++ trees/trie.js
@@ -50,4 +50,45 @@ class Trie {
     }
     return true;
   }
+
+  /**
+   * Remove only the unique suffixes of word
+   * Be mindful of cases where:
+   *  - the last character of word has children
+   *  - characters other than the last have children or complete a string in the
+   *    trie (other than word)
+   *  - no characters of word have children i.e the entirety of the word's path
+   *    through trie can be removed
+   */
+  remove(word) {
+    let node = this.root;
+    let suffixes = [];
+
+    // case where no part of word can be removed from trie
+    for(let i=0; i < word.length; i++) {
+      let currentLetter = word[i];
+      if(node.children[currentLetter]) {
+        node = node.children[currentLetter];
+        suffixes.unshift(node);
+        if(i === word.length-1 && Object.keys(node.children).length) {
+          throw new Error(`Suffixes in trie depend on "${word}"`);
+        }
+      }
+    }
+
+    // case where parts of word can be removed from trie
+    for(let j=1; j < suffixes.length; j++) {
+      let parent = suffixes[j];
+      let child = word[suffixes.length - j];
+      delete parent.children[child];
+      if(parent.end_of_word || Object.keys(parent.children).length) {
+        return `some suffixes of "${word}" removed from trie`;
+      }
+    }
+
+    // case where all parts of word can be removed from trie
+    this.root.children[word[0]];
+    return `removed "${word}", no other "${word[0]}" remaining`;
+
+  }
 }
