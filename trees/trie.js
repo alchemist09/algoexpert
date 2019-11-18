4f6e3bd1b881930f08d5481ab87a718f6f4612b1
Luke Olali <mugapedia@gmail.com>
2019-11-18 22:46:23 +0300

    Defines method for looking up a word in the trie

--- trees/trie.js
+++ trees/trie.js
@@ -31,4 +31,23 @@ class Trie {
     }
     node.end_of_word = true;
   }
+
+  /**
+   * Iterate through word
+   * Map each character in word to a path in trie
+   * If a full path exists, the trie contains the word,
+   * otherwise it doesn't
+   */
+  find(word) {
+    let node = this.root;
+    for(let i=0; i<word.length; i++) {
+      let currentLetter = word[i];
+      if(node.children[currentLetter]) {
+        node = node.children[currentLetter];
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
 }
