061e388529ad382768e0e76aa039dc0e63b1734b
Luke Olali <mugapedia@gmail.com>
2019-11-19 23:02:59 +0300

    Adds problem description

---
+++ dynamic_programming/minimum_cost_path.js
@@ -0,0 +1,6 @@
+/**
+ * There are N stations in a route, starting from 0 to N-1. A train moves from 
+ * station (0) to the last station (N-1) in only forward direction. The cost of
+ * ticket between any two station is given. Find the minimum cost of travel between
+ * station (0) to station (N-1)
+ */
