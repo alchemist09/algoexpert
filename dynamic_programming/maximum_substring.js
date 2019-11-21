c4d9f5df68060aece6a7bb234f3efd099b9fbee0
Luke Olali <mugapedia@gmail.com>
2019-11-21 23:03:30 +0300

    Adds problem description for maximum substring algorithm

---
+++ dynamic_programming/maximum_substring.js
@@ -0,0 +1,18 @@
+/**
+ * Find the length of longest substring of a given string of digits, such
+ * that sum of digits in first half and second half  of the substring is same.
+ * Example:
+ *       Input: 142124
+ *       Output: 6
+ * 
+ * The whole string is the answer, because sum of first three digits = sum of last
+ * three digits (1+4+2 = 1+2+4)
+ * 
+ *      Input:  9430723
+ *      Output: 4
+ * 
+ * longest substring with first and second half having equal sum is "4307".
+ */
+
+ 
+
