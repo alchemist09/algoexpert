e86f3aa71c01b4278069ba9875cc9d459414a8e3
Luke Olali <mugapedia@gmail.com>
2019-11-25 21:28:22 +0300

    Adds description of the maximum subarray sum problem

---
+++ dynamic_programming/maximum_subarray_sum.js
@@ -0,0 +1,9 @@
+/**
+ Given an array of integers, write a function that returns the maximum sum
+ of sub array such that items are contiguous
+
+    Input Array: {-2, -3, 4, -1, -2, 1, 5, -3}
+    Output: 7
+    maximum subarray: {4, -1, -2, 1, 5}
+*/
+
