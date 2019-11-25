4585b0310b1faf5c20c07e436bb87fdecd0002b5
Luke Olali <mugapedia@gmail.com>
2019-11-25 23:07:06 +0300

    Adds description of the string interleaving problem

---
+++ dynamic_programming/string_interleaving.js
@@ -0,0 +1,13 @@
+/**
+ String C is said to be an intervleaving of string A and B if it contains all
+ the characters from A and B, and the relative order of characters of both of 
+ the strings is preserved in C. For example:
+      A = xyz
+      B = abcd
+      C = xabyczd (intervleaving of A and B)
+
+      A = bcc
+      B = bbca
+      C = bbcbcac (intervleaving of A and B)
+*/
+
