35ad04ba8c4b6d153bfd25ac3d0f4e34d9d42305
Luke Olali <mugapedia@gmail.com>
2019-11-25 23:14:36 +0300

    Defines function that checks if a string satisfies interleaving property using brute force approach

--- dynamic_programming/string_interleaving.js
+++ dynamic_programming/string_interleaving.js
@@ -11,3 +11,41 @@
       C = bbcbcac (intervleaving of A and B)
 */
 
+/**
+ * Brute Force solution takes exponential time O(2^N)
+ * @param {string} A - The first string
+ * @param {string} B - The second string
+ * @param {string} C - The interleaved string of strings A and B
+ * @returns {boolean} - Returns true if C is an interleaving of A and B, otherwise false
+ */
+function isInterleaving(A, B, C) {
+  // if all strings are empty
+  if(!A && !B && !C) {
+    return true;
+  }
+
+  // if C is empty, A or B, or both are not empty
+  if(!C) {
+    return false;
+  }
+
+  // if both A and B are empty but C is not
+  if(!A && !B) {
+    return false;
+  }
+
+  let first = false
+  let second = false;
+
+  // if first character of A == first character of C
+  if(A[0] == C[0]) {
+    first = isInterleaving(A.slice(1), B, C.slice(1));
+  }
+
+  // if first character of B == first character of C
+  if(B[0] == C[0]) {
+    second = isInterleaving(A, B.slice(1), C.slice(1));
+  }
+
+  return (first || second);
+}
