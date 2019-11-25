d317ea93117a29b8257969e5a6caa105222624c8
Luke Olali <mugapedia@gmail.com>
2019-11-25 23:23:22 +0300

    Defines function to check whether a string is an interleaving of two other strings using bottom-up dynamic programming

--- dynamic_programming/string_interleaving.js
+++ dynamic_programming/string_interleaving.js
@@ -63,3 +63,53 @@ function createMatrix(x, y) {
   }
   return arr;
 }
+
+/**
+ * Dynamic Programming solution of the string interleaving problem
+ * @param {string} A - The first string
+ * @param {string} B - The second string
+ * @param {string} C - String to check whether it's an interleaving of A and B
+ * @returns {boolean} - Returns true if C is and interleaving of A and B, false otherwise
+ */
+function isInterleavingDP(A, B, C) {
+  if(C.length != (A.length + B.length)) {
+    return false;
+  }
+  const tbl = createMatrix(A, B);
+  tbl[0][0] = true;
+
+  // first row, case where A is empty
+  for(let j=1; j <= B.length; j++) {
+    if(B[j-1] != C[j-1]) {
+      tbl[0][j] = false;
+    } else {
+      tbl[0][j] = tbl[0][j-1];
+    }
+  }
+
+  // first column, case where B is empty
+  for(let i=1; i <= A.length; i++) {
+    if(A[i-1] != C[i-1]) {
+      tbl[i][0] = false;
+    } else {
+      tbl[i][0] = tbl[i-1][0];
+    }
+  }
+
+  for(let i=1; i <= A.length; i++) {
+    for(let j=1; j <= B.length; j++) {
+      let k = i + j;
+      if(A[i-1] == C[k-1]) {
+        tbl[i][j] = tbl[i-1][j];
+      } else if(B[j-1] == C[k-1]) {
+        tbl[i][j] = tbl[i][j-1];
+      } else {
+        tbl[i][j] = false;
+      }
+      // tbl[i][j] = (A[i-1] == C[k-1] ? tbl[i-1][j] : false) || 
+      //             (B[j-1] == C[k-1] ? tbl[i][j-1] : false);
+    }
+  }
+  
+  return tbl[A.length][B.length];
+}
