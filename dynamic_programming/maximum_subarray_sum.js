c5d40734327d470cbed52b965a6a5082e9764bab
Luke Olali <mugapedia@gmail.com>
2019-11-25 21:32:15 +0300

    Defines function to solve the problem using brute force approach

--- dynamic_programming/maximum_subarray_sum.js
+++ dynamic_programming/maximum_subarray_sum.js
@@ -7,3 +7,23 @@
     maximum subarray: {4, -1, -2, 1, 5}
 */
 
+/**
+ * Brute Force Approach
+ * O(n^2) runtime
+ */
+function maxSubArraySum(arr) {
+  let maxSum = 0;
+  let tempSum = 0;
+  const length = arr.length;
+  for(let i=0; i < length; i++) {
+    // temp sum holds the sum of elements from i to j index (including both)
+    tempSum = arr[i];
+    for(let j=i+1; j < length; j++) {
+      tempSum += arr[j];
+      if(tempSum > maxSum) {
+        maxSum = tempSum;
+      }
+    }
+  }
+  return maxSum;
+}
