f26443ce332a239061fbd1687abc5179610a00a9
Luke Olali <mugapedia@gmail.com>
2019-11-25 21:24:35 +0300

    Defines function to solve the problem using bottom-up dynamic programming approach

--- dynamic_programming/ways_to_score.js
+++ dynamic_programming/ways_to_score.js
@@ -34,3 +34,29 @@ function waysToScore(n) {
 
   return waysToScore(n-10) + waysToScore(n-5) + waysToScore(n-3);
 }
+
+
+/**
+ * Bottom-up approach using dynamic programming
+ * @param {number} n - The target score
+ * @returns {number} - Count of the different ways to score
+ */
+function waysToScore2(n) {
+  const arr = new Array(n+1).fill(0);
+  arr[0] = 1
+  for(let i=1; i <= n; i++) {
+    if(i-3 >= 0) {
+      arr[i] = arr[i-3] + arr[i];
+    } 
+    
+    if(i-5 >= 0) {
+      arr[i] = arr[i-5] + arr[i];
+    } 
+
+    if(i-10 >= 0) {
+      arr[i] = arr[i-10] + arr[i];
+    }
+  }
+    
+  return arr[n];
+}
