b16c76d750346ddbde53c5fd5112ed0bf30476b1
Luke Olali <mugapedia@gmail.com>
2019-11-26 22:29:08 +0300

    Adds problem description for the subset sum problem

---
+++ dynamic_programming/subset_sum.js
@@ -0,0 +1,8 @@
+/**
+ Given an array of non-negative integers and a positive number X, determine if 
+ there exists a subset of elements of array with sum equal to X.
+ For example:
+            Input array: {3, 2, 7, 1}    X = 6
+            Output: True  // because sum of {3, 2, 1} is 6
+*/
+
