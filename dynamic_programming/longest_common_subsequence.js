1ba32eafa0dbc47f3e5a5043c3c22667aac3fd0f
Luke Olali <mugapedia@gmail.com>
2019-11-26 22:49:35 +0300

    Adds description for the longest common susbsequence problem

---
+++ dynamic_programming/longest_common_subsequence.js
@@ -0,0 +1,12 @@
+/**
+ A subsequence of a string S is a set of characters that appear in the string
+ in left-to-right order, but not necessarily consecutively. For example, if 
+ string is ACTTGCG, 
+      then, ACT, ATTC, T, ACTTCG are all subsequences
+      but TTA is not a subsequence of the string
+
+ A common subsequence of two strings is a subsequence such that it's a subsequence
+ of both strings. A longest common subsequence (LCS) is a common subsequence of
+ maximal length     
+*/
+
