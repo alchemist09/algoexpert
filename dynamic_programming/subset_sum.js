552149665e0dca41d3a574a26806438fbcda53c6
Luke Olali <mugapedia@gmail.com>
2019-11-26 22:40:58 +0300

    Defines function that uses dynamic programming to solve subset sum problem

--- dynamic_programming/subset_sum.js
+++ dynamic_programming/subset_sum.js
@@ -42,3 +42,34 @@ function createMatrix(arr, target) {
   }
   return table;
 }
+
+/**
+ * Dynamic Programming Solution O(N^2)
+ * @param {*} arr - Array of non-negative integers
+ * @param {*} target - The target sum
+ * @returns {boolean} - Returns true if sum of subset of arr adds up to target, false otherwise
+ */
+function isSubsetSumDP(arr, target) {
+  const T = createMatrix(arr, target);
+  
+  // if target is 0, then answer is true
+  for(let i=0; i <= arr.length; i++) {
+    T[i][0] = true;
+  }
+
+  for(let j=1; j <= target; j++) {
+    T[0][j] = false; 
+  }
+
+  for(let i=1; i <= arr.length; i++) {
+    for(let j=1; j <= target; j++) {
+      if(j - arr[i-1] >= 0) {
+        T[i][j] = T[i-1][j] || T[i-1][j-arr[i-1]];
+      } else {
+        T[i][j] = T[i-1][j];
+      }
+    }
+  }
+
+  return T[arr.length][target];
+}
