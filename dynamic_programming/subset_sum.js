9428745dae8be20b3a5de7bb142f21d0ab2a552f
Luke Olali <mugapedia@gmail.com>
2019-11-26 22:33:52 +0300

    Defines function to solve the subset sum problem using a brute force recursive approach

--- dynamic_programming/subset_sum.js
+++ dynamic_programming/subset_sum.js
@@ -6,3 +6,25 @@
             Output: True  // because sum of {3, 2, 1} is 6
 */
 
+/**
+ * Recursive solution, O(2^N)
+ * @param {array} arr - Array of non-negative integers
+ * @param {number} target - The target sum
+ * @returns {boolean} - Returns true if the sum of subset of arr adds up to target, false otherwise
+ */
+function isSubsetSum(arr, target) {
+  const len = arr.length;
+  if(target == 0) {
+    return true;
+  }
+
+  if(len == 0) {
+    return false;
+  }
+
+  if(arr[0] > target) {
+    return isSubsetSum(arr.slice(1), target);
+  }
+
+  return isSubsetSum(arr.slice(1), target) || isSubsetSum(arr, target-arr[0]);
+}
