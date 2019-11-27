55180f6389e17f97393e5574cb0990be539ea0fb
Luke Olali <mugapedia@gmail.com>
2019-11-27 22:44:41 +0300

    Adds description for the egg dropping problem

---
+++ dynamic_programming/egg_dropping.js
@@ -0,0 +1,10 @@
+/**
+ Given a number of eggs and a multi-story building, find the minimum number of 
+ egg drops that one will have to perform in the worst case to be able to know
+ the floor from which the eggs start breaking. For example, if we only have 
+ one egg, and 10 floors, the minimum number of drops we'll need to perform to 
+ determine the highest floor from which eggs don't break is 10. That is we'll have
+ to start from the first floor and move linearly upward dropping the egg from 
+ every floor until it breaks or we reach the 10th floor without breaking
+*/
+
