5ba3559b6c75c61e36e09b7735bc60d853c99419
Luke Olali <mugapedia@gmail.com>
2019-11-27 22:35:53 +0300

    Adds description for the longest palindromic subsequence problem

---
+++ dynamic_programming/longest_palindromic_subsequence.js
@@ -0,0 +1,6 @@
+/**
+ Given a string, find the longest subsequence that is also a palindrome. For example,
+ if the string is BBABCBCAB, then its subsequence BABCBAB is the longest subsequence
+ that is also a palindrome, so the answer is length of BABCBAB which is 7
+*/
+
