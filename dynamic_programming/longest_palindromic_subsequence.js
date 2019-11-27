06929b7b5e7984479d3c57e5e46ac95cfdeec49b
Luke Olali <mugapedia@gmail.com>
2019-11-27 22:40:48 +0300

    Defines function to solve longest palindromic subsequence problem using dynamic programming

--- dynamic_programming/longest_palindromic_subsequence.js
+++ dynamic_programming/longest_palindromic_subsequence.js
@@ -25,3 +25,37 @@ function lpsRec(str) {
     return Math.max(lpsRec(str.slice(start, end)), lpsRec(str.slice(start+1, end+1)));
   }
 }
+
+/**
+ * Using Dynamic Programming to implement the Longest Palindromic Subsequence
+ * Runtime is polynomial O(n^2), Space complexity is O(n^2)
+ * 
+ * @param {string} str The input string
+ */
+function lpsDP(str) {
+  if(!str) { return 0; }
+  const n = str.length;
+  const cache = new Array(n);
+  for(let i=0; i < n; i++) {
+    cache[i] = new Array(n).fill(0);
+  }
+
+  // single length string are palindromes of length 1
+  for(let i=0; i < n; i++) {
+    cache[i][i] = 1;
+  }
+
+  // find palindromes from length 2 to n saving the longest
+  for(let curr_len=2; curr_len <= n; curr_len++) {
+    for(let i=0; i < n - curr_len+1; i++) {
+      let j = i + curr_len-1;
+      if(str.charAt(i) == str.charAt(j)) {
+        cache[i][j] = 2 + cache[i+1][j-1];
+      } else {
+        cache[i][j] = Math.max(cache[i][j-1], cache[i+1][j]);
+      }
+    }
+  }
+  
+  return cache[0][n-1];
+}
