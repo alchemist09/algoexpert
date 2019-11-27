942fdcfe3bb1cfc1506c43c3b52b0edaa7b5de34
Luke Olali <mugapedia@gmail.com>
2019-11-27 21:14:18 +0300

    Defines function to solve the coin change problem using a recursive brute force approach

--- dynamic_programming/coin_change.js
+++ dynamic_programming/coin_change.js
@@ -12,3 +12,22 @@
     Output: 2 { 5 + 6 } 
 */
 
+/**
+ * Recursive Brute Force Solution
+ * @param {number} S - The sum total of change to be given
+ * @param {array} coints - Array containing denominations of the various coins
+ * @returns {number} - The minimum no. of coins required to give change for a particular denomination
+ */
+function minCoins(S, coins) {
+  if(S == 0) { return 0 };
+  let numCoins = Number.MAX_VALUE;
+  for(let i=0; i < coins.length; i++) {
+    if(coins[i] <= S) {
+      let temp = minCoins(S-coins[i], coins);
+      if(temp+1 < numCoins) {
+        numCoins = temp + 1;
+      }
+    }
+  }
+  return numCoins;
+}
