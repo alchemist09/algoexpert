003c99df2c94b3d71b511ce6754d51d69d010e2e
Luke Olali <mugapedia@gmail.com>
2019-11-27 21:10:30 +0300

    Adds problem description for coin change problem

---
+++ dynamic_programming/coin_change.js
@@ -0,0 +1,14 @@
+/**
+ Given an infinite supply of coins of given denominations (values), (V1, V2,.., Vn),
+ Find the minimum number of coins that sum up to a value S. For example:
+
+    Input: Coins = { 1, 2, 5, 10, 20, 50 } S = 65
+    Output: 3 { 50 + 10 + 5 }
+
+    Input Coins = { 1, 2, 5, 10, 12, 20, 50, }  S = 65
+    Output: 3 { 50 + 10 + 5 }
+
+    Input Coins = { 1, 5, 6, 9 }  S = 11
+    Output: 2 { 5 + 6 } 
+*/
+
