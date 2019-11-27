30d4210cf608f66addca2011e7bc6f9ca9385949
Luke Olali <mugapedia@gmail.com>
2019-11-27 22:50:48 +0300

    Defines function to solve egg dropping problem using brute force recursive approach

--- dynamic_programming/egg_dropping.js
+++ dynamic_programming/egg_dropping.js
@@ -8,3 +8,30 @@
  every floor until it breaks or we reach the 10th floor without breaking
 */
 
+/**
+ * Naive recursive implementation of egg drop solution
+ * 
+ * @param {eggs} int The number of eggs to start with
+ * @param {number} floors The number of floors in the buiding we're dropping eggs from
+ * @return {number} Minimum no. of drops needed to find breaking floor in worst case
+ */
+function eggDropRec(eggs, floors) {
+  // if we only have one egg or we have zero/one floors, return no. of floors
+  if(floors==1 || floors==0 || eggs==1) {
+    return floors;
+  }
+
+  let minDrops = Number.MAX_VALUE;
+
+  for(let currFloor=1; currFloor <= floors; currFloor++) {
+    let temp = Math.max(
+      eggDropRec(eggs-1, currFloor-1),
+      eggDropRec(eggs, floors-currFloor)
+    )
+
+    if(temp < minDrops) {
+      minDrops = temp;
+    }
+  }
+  return minDrops + 1;
+}
