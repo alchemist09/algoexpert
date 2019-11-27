e6b2520d4260c7cc7d142733b7c3ac93eee28b98
Luke Olali <mugapedia@gmail.com>
2019-11-27 22:22:50 +0300

    Adds description for the knapsack problem

---
+++ dynamic_programming/knapsack.js
@@ -0,0 +1,7 @@
+/**
+ Given a set of items, each with a weight and a value associated with it, determine
+ the maximum value you can obtain by including items in the knapsack in such a way 
+ that the total weight of the items is less than on equal to the maximum capacity
+ of the knapsack.
+*/
+
